d2df995e0048bf4d4e91eff748b498926ae190b9
Lank1906 <buuixuanhoangc@gmail.com>
2024-08-08 13:42:46 +0700

    del hieu

---
+++ js/login.js
@@ -0,0 +1,16 @@
+import "./action.js"
+function Login(email,password){
+    console.log(getFetch('../data.json'))
+}
+function LayThongTinTaiKhoan(){
+    const ttts=getFetch('../data.json')
+}
+function LayThongTinGiaoDich(){
+    const ttgd=getFetch('../data.json')
+}
+function ChangePassword(){
+    const tttk=getFetch('../data.json')
+}
+function Search(){
+    const nds=getFetch('../data.json')
+}
